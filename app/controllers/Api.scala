0d76acf2f1c3b12ffd6b09650ea3b21010c7b68f
Cobbleopolis <cobbleopolis@gmail.com>
2016-02-14 11:25:59 -0500

    fixed bug in auth endpoint

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -1,23 +1,21 @@
 package controllers
 
-import controllers.Application._
-import models.User
+import play.api.Play.current
 import play.api.db.DB
 import play.api.libs.json.{JsObject, JsString}
-import play.api.libs.ws.WSResponse
 import play.api.mvc.{Action, Controller}
-import reference.{DBReference, AuthReference}
+import reference.{AuthReference, DBReference}
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.Play.current
 
 object Api extends Controller {
 
 	def auth = Action.async { request =>
-		AuthReference.googleAuth.withQueryString("id_token" -> request.getQueryString("id").get).get().map{ response =>
-//			if ((response.json \ "aud").toString == AuthReference.googleClientId)
+		AuthReference.googleAuth.withQueryString("id_token" -> request.getQueryString("id").get).get().map { response =>
+			if ((response.json \ "aud").as[String] == AuthReference.googleClientId)
 				Ok(JsObject(Seq("redirect" -> JsString("/user/" + request.getQueryString("name").get))))
-//			else
-//				Ok(JsObject(Seq("redirect" -> JsString("/"))))
+			else
+				Ok(JsObject(Seq("redirect" -> JsString("/login"))))
 		}
 	}
 
